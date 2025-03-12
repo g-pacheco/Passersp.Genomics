ff4ae70040e696cde0a71c3f3fde23df3a9b5c04
sbbe-oficial <37849281+g-pacheco@users.noreply.github.com>
2025-03-12 16:29:26 +0100

    Minor.

---
+++ Passersp.Genomics--Pipeline/Passersp.Genomics--Plots/Passersp.Genomics--Kinship/Y150239Genomics--Kinship.R
@@ -0,0 +1,180 @@
+### The BEGINNING ~~~~~
+##
+# Y150239Genomics--Kinship by George Pacheco ~
+
+
+# Cleans the environment ~ 
+rm(list=ls())
+
+
+# Sets working directory ~
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+
+# Loads required packages ~
+pacman::p_load(optparse, tidyverse, plyr, RColorBrewer, extrafont, ggforce, ggstar, ggrepel, RcppCNPy, reshape2,
+               gridExtra, grid, ggpubr, rphylopic, viridis, forcats)
+
+
+# Loads data while loading accompanying annotations ~
+annot <- list()
+rab <- list()
+annotL <- dir(pattern = ".labels")
+rabL <- dir(pattern = ".res")
+
+for (k in 1:length(annotL)) {
+  annot[[k]] <- read.table(annotL[k], sep = "\t", header = FALSE, stringsAsFactors = FALSE)
+  colnames(annot[[k]]) <- c("Ind1")
+  annot[[k]]$Ind2 <- c(annot[[k]]$Ind1[-c(1)], NA)
+  annot[[k]]$Population <- gsub("[A-z._]*(Autosomes|Allosome).", "", annotL[k])
+  annot[[k]]$Population <- gsub(".labels", "", annot[[k]]$Population)
+  annot[[k]]$SubPopulation <- annot[[k]]$Population
+  
+  ### Only needed because the population Utrecht has subpopulation ~
+  annot[[k]] <- annot[[k]] %>%
+    mutate(SubPopulation = case_when(Ind1 %in% c("PD22NLD0146F", "PD22NLD0147F") ~ "Garderen",
+                                     Ind1 == "PDOM2022NLD0077M" ~ "Meerkerk",
+                                     Ind1 == "PI22NLD0001M" ~ "Focal Ind.", TRUE ~ Population))
+  annot[[k]]$CHRType <- str_extract(annotL[k], "(Allosome|Autosomes)")
+  annot[[k]] <- annot[[k]] %>%
+    group_by(SubPopulation) %>% mutate(Ind1b = paste(SubPopulation, sprintf("%02d", row_number()), sep = "_")) %>%
+    ungroup() %>%
+    mutate(Ind2b = lead(Ind1b, default = paste0(SubPopulation[1], "_", sprintf("%02d", n() + 1))))
+  rab[[k]] <- read.table(rabL[k], header = TRUE, stringsAsFactors = FALSE)
+  rab[[k]]$a <- rab[[k]]$a + 1
+  rab[[k]]$Population <- annot[[k]]$Population[match(rab[[k]]$a, seq_along(annot[[k]]$Ind1))]
+  rab[[k]]$CHRType <- str_extract(rabL[k], "(Allosome|Autosomes)")
+  rab[[k]]$Ind1 <- annot[[k]]$Ind1b[match(rab[[k]]$a, seq_along(annot[[k]]$Ind1))]
+  rab[[k]]$Ind2 <- annot[[k]]$Ind2b[match(rab[[k]]$b, seq_along(annot[[k]]$Ind1))]
+  rab[[k]]$a <- annot[[k]]$Ind1[match(rab[[k]]$a, seq_along(annot[[k]]$Ind1))]
+  rab[[k]]$b <- annot[[k]]$Ind2[match(rab[[k]]$b, seq_along(annot[[k]]$Ind1))]
+  rab[[k]]$Pair <- paste(rab[[k]]$Ind1, "Vs", rab[[k]]$Ind2)
+  
+  # Only needed because the individual Meerkerk_01 is amidst the Utrecht subpopulation ~
+  rab[[k]] <- rab[[k]] %>% mutate(Invert = str_detect(Ind2, "Meerkerk") & !Ind1 %in% c("Garderen_01", "Garderen_02"),
+                                  Temp_Ind1 = Ind1,
+                                  Temp_Ind2 = Ind2,
+                                  Ind1 = ifelse(Invert, Temp_Ind2, Temp_Ind1),
+                                  Ind2 = ifelse(Invert, Temp_Ind1, Temp_Ind2),
+                                  Pair = paste(Ind1, "Vs", Ind2)) %>%
+                                  select(-Invert, -Temp_Ind1, -Temp_Ind2)
+  rab[[k]] <- rab[[k]] %>% mutate(Invert = str_detect(Ind2, "Focal Ind."),
+                                  Temp_Ind1 = Ind1,
+                                  Temp_Ind2 = Ind2,
+                                  Ind1 = ifelse(Invert, Temp_Ind2, Temp_Ind1),
+                                  Ind2 = ifelse(Invert, Temp_Ind1, Temp_Ind2),
+                                  Pair = paste(Ind1, "Vs", Ind2)) %>%
+                                  select(-Invert, -Temp_Ind1, -Temp_Ind2)
+  tryCatch({rab[[k]] <- rab[[k]] %>%
+            select(Ind1, Ind2, a, b, rab, Pair, CHRType, Population)}, error = function(e) {
+            print(paste("Error in rab[[", k, "]] column selection:", e$message))})
+  rab[[k]] <- rab[[k]] %>%
+              arrange(Ind1, Ind2)}
+
+
+# Expands rab list ~
+fulldf <- bind_rows(rab)
+
+
+# Fixes individual Y150239 ~
+fulldf <- fulldf %>% mutate(across(c(Ind1, Ind2, Pair), ~ str_replace_all(., "Focal Ind._01", "Focal Ind.")))
+
+
+# Corrects Population names ~
+levels(fulldf$Population <- sub("TreeSparrow", "Tree Sparrow", fulldf$Population))
+levels(fulldf$Population <- sub("Utrecht", "Focal Area", fulldf$Population))
+
+
+# Reorders Population ~
+fulldf$Population <- factor(fulldf$Population, ordered = T,
+                            levels = c("Focal Area",
+                                       "Sales",
+                                       "Crotone",
+                                       "Guglionesi", 
+                                       "Lesina",
+                                       "Chokpak",
+                                       "Tree Sparrow"))
+
+# Creates plot (Heatmap) ~
+Kinship_Plot_Heatmap <-
+  ggplot(subset(fulldf, CHRType == "Autosomes"), aes(Ind1, y = Ind2, fill = rab)) + 
+  geom_tile(colour = "#000000") +
+  scale_fill_continuous(low = "#ffffff", high = "#f768a1") +
+  scale_x_discrete(expand = c(0, 0)) +
+  scale_y_discrete(limits = rev, expand = c(0, 0)) +
+  facet_wrap(Population ~., scales = "free", ncol = 2) +
+  theme(panel.background = element_rect(fill = "#ffffff"),
+        panel.border = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.spacing = unit(.2, "cm"),
+        legend.position = "right",
+        legend.key = element_blank(),
+        legend.background = element_blank(),
+        legend.margin = margin(t = 0, b = 0, r = 15, l = 15),
+        legend.box = "vertical",
+        legend.box.margin = margin(t = 20, b = 30, r = 0, l = 0),
+        axis.title = element_blank(),
+        axis.text.x = element_text(family = "Optima", color = "#000000", size = 8, face = "bold", angle = 45, vjust = 1, hjust = 1),
+        axis.text.y = element_text(family = "Optima", color = "#000000", size = 8, face = "bold"),
+        axis.ticks = element_line(color = "#000000", linewidth = .3),
+        strip.text = element_text(family = "Optima", colour = "#000000", size = 13, face = "bold"),
+        strip.background = element_rect(colour = "#000000", fill = "#d6d6d6", linewidth = .3),
+        axis.line = element_line(colour = "#000000", linewidth = .3)) +
+  guides(fill = guide_legend(title = "Rab", title.theme = element_text(family = "Optima", size = 16, face = "bold"),
+                             label.theme = element_text(family = "Optima", size = 15), reverse = TRUE))
+
+
+# Saves plot (Heatmap) ~
+ggsave(Kinship_Plot_Heatmap, file = "Y150239Genomics--Kinship.pdf",
+       device = cairo_pdf, limitsize = FALSE, scale = 1, width = 12, height = 14, dpi = 600)
+ggsave(Kinship_Plot_Heatmap, file = "Y150239Genomics--Kinship.jpeg",
+       limitsize = FALSE, scale = 1, width = 12, height = 14, dpi = 600)
+
+
+# Creates plot (Boxplot) ~
+Kinship_Plot_Boxplot <-
+  ggplot(fulldf, aes(x = Population, y = rab)) +
+  geom_boxplot(fill = "#ffffff", colour = "#000000", show.legend = FALSE, linewidth = .285, width = .15, fatten = 1,
+               outlier.shape = 21, outlier.fill = "#005824", outlier.colour = "#000000", outlier.stroke = .2, outlier.alpha = .9) +
+  geom_point(data = subset(fulldf, rab >= .125), shape = 21, fill = "#df65b0", colour = "#000000", stroke = .2, alpha = .9) +
+  geom_hline(yintercept = .125, linetype = "twodash", color = "#df65b0", linewidth = .3, alpha = .9) +
+  geom_label_repel(data = subset(fulldf, rab >= .125), aes(label = Pair),
+                   size = 3, nudge_x = .1, family = "Optima", fontface = "bold") +
+  scale_x_discrete(expand = c(.05, .05)) +
+  scale_y_continuous ("Rab", 
+                      breaks = c(.1, .2, .3, .4, .5), 
+                      labels = c("0.1", "0.2", "0.3", "0.4", "0.5"),
+                      limits = c(0, .52),
+                      expand = c(.005, .005)) +
+  theme(panel.background = element_rect(fill = "#ffffff"),
+        panel.border = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        legend.position = "right",
+        legend.key = element_blank(),
+        legend.background = element_blank(),
+        legend.margin = margin(t = 0, b = 0, r = 15, l = 15),
+        legend.box = "vertical",
+        legend.box.margin = margin(t = 20, b = 30, r = 0, l = 0),
+        axis.title.x = element_blank(),
+        axis.title.y = element_text(family = "Optima", color = "#000000", size = 12, face = "bold", margin = margin(t = 0, r = 20, b = 0, l = 0)),
+        axis.text.x = element_text(family = "Optima", color = "#000000", size = 10, face = "bold", angle = 45, vjust = 1, hjust = 1),
+        axis.text.y = element_text(family = "Optima", color = "#000000", size = 9, face = "bold"),
+        axis.ticks = element_line(color = "#000000", linewidth = .3),
+        strip.text = element_text(family = "Optima", colour = "#000000", size = 14, face = "bold"),
+        strip.background = element_rect(colour = "#000000", fill = "#d6d6d6", linewidth = .3),
+        axis.line = element_line(colour = "#000000", linewidth = .3)) +
+  guides(fill = guide_legend(title = "Rab", title.theme = element_text(family = "Optima", size = 16, face = "bold"),
+                             label.theme = element_text(family = "Optima", size = 15)))
+
+
+# Saves plot (Boxplot) ~
+ggsave(Kinship_Plot_Boxplot, file = "Y150239Genomics--Kinship_Boxplot.pdf",
+       device = cairo_pdf, limitsize = FALSE, scale = 1, width = 8, height = 5, dpi = 600)
+ggsave(Kinship_Plot_Boxplot, file = "Y150239Genomics--Kinship_Boxplot.png",
+       limitsize = FALSE, scale = 1, width = 8, height = 5, dpi = 600)
+
+#
+##
+### The END ~~~~~
---
+++ Passersp.Genomics--Pipeline/Passersp.Genomics--Plots/Passersp.Genomics--ngsAdmix/Y150239Genomics--ngsAdmix.R
@@ -0,0 +1,272 @@
+### The BEGINNING ~~~~~
+##
+# ~ Plots Passer sp. Genomics -- ngsAdmix | Written by George Pacheco.
+
+
+# Cleans the environment ~ 
+rm(list=ls())
+
+
+# Sets working directory ~
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+
+# Loads required packages ~
+pacman::p_load(tidyverse, ggpattern, ggh4x, scales, optparse, plyr, RColorBrewer, extrafont, gtable, grid, ggtext, glue)
+
+
+# Creates colour palette ~
+nb.cols <- 15
+MyColours <- colorRampPalette(brewer.pal(8, "Paired"))(nb.cols)
+
+
+# Loads the data ~
+samples.auto <- read.table("Y150239Genomics--ngsAdmix.Autosomes.popfile", stringsAsFactors = FALSE, sep = "\t")
+samples.allo <- read.table("Y150239Genomics--ngsAdmix.Allosome.popfile", stringsAsFactors = FALSE, sep = "\t")
+
+
+# Reads the annotation file ~
+ids.auto <- read.table("./Autosomes/AllSamples_bcftools.raw.vcf.Filtered.Autosomes.NoKinship.NoTreeSparrow.MAFfiltered.Pruned.K2.labels",
+                       stringsAsFactors = FALSE, sep = "\t", header = FALSE, col.names = c("Sample_ID"))
+ids.allo <- read.table("./Allosome/AllSamples_bcftools.raw.vcf.Filtered.Allosome.NoKinship.NoTreeSparrow.MAFfiltered.Pruned.K2.labels",
+                       stringsAsFactors = FALSE, sep = "\t", header = FALSE, col.names = c("Sample_ID"))
+
+
+# Expands ids.auto by adding Population ~
+ids.auto$Population <- ifelse(grepl("FR0", ids.auto$Sample_ID), "Sales",
+                       ifelse(grepl("KAZ", ids.auto$Sample_ID), "Chokpak",
+                       ifelse(grepl("Lesina", ids.auto$Sample_ID), "Lesina",
+                       ifelse(grepl("Crotone", ids.auto$Sample_ID), "Crotone",
+                       ifelse(grepl("Guglionesi", ids.auto$Sample_ID), "Guglionesi",
+                       ifelse(grepl("PI22NLD0001M", ids.auto$Sample_ID), "Focal Ind.",
+                       ifelse(grepl("PD22NLD0146F", ids.auto$Sample_ID), "Garderen",
+                       ifelse(grepl("PD22NLD0147F", ids.auto$Sample_ID), "Garderen",
+                       ifelse(grepl("PDOM2022NLD0077M", ids.auto$Sample_ID), "Meerkerk",
+                       ifelse(grepl("PDOM2022NLD0", ids.auto$Sample_ID), "Utrecht", "Error"))))))))))
+
+
+# Expands ids.allo by adding Population ~
+ids.allo$Population <- ifelse(grepl("FR0", ids.allo$Sample_ID), "Sales",
+                       ifelse(grepl("KAZ", ids.allo$Sample_ID), "Chokpak",
+                       ifelse(grepl("Lesina", ids.allo$Sample_ID), "Lesina",
+                       ifelse(grepl("Crotone", ids.allo$Sample_ID), "Crotone",
+                       ifelse(grepl("Guglionesi", ids.allo$Sample_ID), "Guglionesi",
+                       ifelse(grepl("PI22NLD0001M", ids.allo$Sample_ID), "Focal Ind.",
+                       ifelse(grepl("PDOM2022NLD0077M", ids.allo$Sample_ID), "Meerkerk",
+                       ifelse(grepl("PDOM2022NLD0", ids.allo$Sample_ID), "Utrecht", "Error"))))))))
+
+
+# Reorders Population ~
+ids.auto$Population <- factor(ids.auto$Population, ordered = T,
+                              levels = c("Utrecht",
+                                         "Sales",
+                                         "Crotone",
+                                         "Guglionesi",
+                                         "Lesina",
+                                         "Chokpak",
+                                         "Focal Ind.",
+                                         "Garderen",
+                                         "Meerkerk"))
+
+
+# Reorders Population ~
+ids.allo$Population <- factor(ids.allo$Population, ordered = T,
+                              levels = c("Utrecht",
+                                         "Sales",
+                                         "Crotone",
+                                         "Guglionesi",
+                                         "Lesina",
+                                         "Chokpak",
+                                         "Focal Ind.",
+                                         "Garderen",
+                                         "Meerkerk"))
+
+
+# Expands PCA_Annot by adding Species ~
+ids.auto$Species <- ifelse(ids.auto$Population %in% c("Utrecht", "Sales", "Garderen", "Meerkerk"), "House",
+                    ifelse(ids.auto$Population %in% c("Chokpak", "Lesina"), "Spanish",
+                    ifelse(ids.auto$Population %in% c("Guglionesi", "Crotone"), "Italian",
+                    ifelse(ids.auto$Population %in% c("Y150239"), "Focal Ind.", "Error"))))
+
+
+# Expands PCA_Annot by adding Species ~
+ids.allo$Species <- ifelse(ids.allo$Population %in% c("Utrecht", "Sales", "Meerkerk"), "House",
+                    ifelse(ids.allo$Population %in% c("Chokpak", "Lesina"), "Spanish",
+                    ifelse(ids.allo$Population %in% c("Guglionesi", "Crotone"), "Italian",
+                    ifelse(ids.allo$Population %in% c("Y150239"), "Focal Ind.", "Error"))))
+
+
+# Reorders Population ~
+ids.auto$Species <- factor(ids.auto$Species, ordered = T,
+                           levels = c("House",
+                                      "Italian",
+                                      "Spanish",
+                                      "Focal Ind."))
+
+
+# Reorders Population ~
+ids.allo$Species <- factor(ids.allo$Species, ordered = T,
+                           levels = c("House",
+                                      "Italian",
+                                      "Spanish",
+                                      "Focal Ind."))
+
+
+# Recognises chromosomes ~
+ids.auto$chrtype <- "Autosomes"
+ids.allo$chrtype <- "Chromosome Z"
+
+
+# Creates data frame ~
+fulldf.auto <- data.frame()
+fulldf.allo <- data.frame()
+
+
+x.auto <- list(c(3, 2, 1, 4, 6, 5, 7),
+               c(4, 5, 1, 2, 6, 3),
+               c(4, 2, 5, 3, 1),
+               c(4, 3, 1, 2),
+               c(1, 3, 2),
+               c(1, 2))
+
+
+x.allo <- list(c(1, 2, 3, 4, 5, 6, 7),
+               c(1, 2, 3, 4, 5, 6),
+               c(1, 3, 2, 5, 4),
+               c(2, 4, 1, 3),
+               c(2, 1, 3),
+               c(2, 1))
+
+
+# Defines samples' IDs ~
+sampleid = "Sample_ID"
+
+
+# Loops over all Ks while adding labels and reordering clusters ~
+for (j in 1:length(samples.auto[, 1])){
+  data <- read.table(samples.auto[j, 1])[, x.auto[[j]]]
+  for (i in 1:dim(data)[2]) { 
+    temp <- data.frame(Ancestry = data[, i])
+    temp$K <- as.factor(rep(i, times = length(temp$Ancestry)))
+    temp[sampleid] <- as.factor(ids.auto[sampleid][, 1])
+    temp$K_Value <- as.factor(rep(paste("K = ", dim(data)[2], sep = ""), times = length(temp$Ancestry)))
+    temp <- merge(ids.auto, temp)
+    fulldf.auto <- rbind(fulldf.auto, temp)}}
+
+
+for (j in 1:length(samples.allo[, 1])){
+  data <- read.table(samples.allo[j, 1])[, x.allo[[j]]]
+  for (i in 1:dim(data)[2]) { 
+    temp <- data.frame(Ancestry = data[, i])
+    temp$K <- as.factor(rep(i, times = length(temp$Ancestry)))
+    temp[sampleid] <- as.factor(ids.allo[sampleid][, 1])
+    temp$K_Value <- as.factor(rep(paste("K = ", dim(data)[2], sep = ""), times = length(temp$Ancestry)))
+    temp <- merge(ids.allo, temp)
+    fulldf.allo <- rbind(fulldf.allo, temp)}}
+
+
+# Finds missing rows in fulldf.allo ~ 
+fulldf.allo_missing_rows <- anti_join(fulldf.auto, fulldf.allo, by = c("Sample_ID", "Population", "Species", "K", "K_Value"))
+
+
+# Adds missing rows to fulldf.allo ~
+fulldf.allo_missing_rows <- fulldf.allo_missing_rows %>%
+                            group_by(K_Value) %>%
+                            mutate(Ancestry = ifelse(K == 1, 1, 0),
+                            chrtype = "Chromosome Z",
+                            Status = "Missing") %>%
+                            ungroup()
+
+
+# Adds Status in fulldf.allo_present_rows ~ 
+fulldf.allo_present_rows <- fulldf.allo %>%
+                            mutate(Status = "Present")
+
+
+# Combines fulldf.allo_present_rows with fulldf.allo_missing_rows ~ 
+fulldf.allo <- bind_rows(fulldf.allo_missing_rows, fulldf.allo_present_rows)
+
+
+# Adds Status in full.auto ~
+fulldf.auto <- fulldf.auto %>%
+               mutate(Status = "Present")
+
+
+# Combines fulldf.auto with fulldf.allo ~
+fulldf <- rbind(fulldf.auto, fulldf.allo)
+
+
+# Expands fulldf by adding Group ~
+fulldf$Group <- ifelse(fulldf$Population %in% c("Utrecht", "Sales"), "House Sparrow",
+                ifelse(fulldf$Population %in% c("Guglionesi", "Crotone"), "Italian Sparrow",
+                ifelse(fulldf$Population %in% c("Chokpak", "Lesina"), "Spanish Sparrow",
+                ifelse(fulldf$Population %in% c("Focal Ind.", "Meerkerk", "Garderen"), "Focal Area", "Error"))))
+
+
+# Reorders Population ~
+fulldf$Group <- factor(fulldf$Group, ordered = T,
+                           levels = c("House Sparrow",
+                                      "Italian Sparrow",
+                                      "Spanish Sparrow",
+                                      "Focal Area"))
+
+
+# Reorders chrtype ~
+fulldf$chrtype <- factor(fulldf$chrtype, ordered = T,
+                        levels = c("Autosomes",
+                                   "Chromosome Z"))
+
+
+# Defines the target to be plotted ~
+target = "Population"
+
+
+# Define colors for Present status based on K
+color_palette <- c("#ee0000", "#1E90FF", "#FFD700", "#88419d", "#c994c7", "#FF6347", "#00BFFF")
+
+
+# Sets bar fill colour ~
+fulldfUp <- fulldf %>%
+            mutate(K = as.factor(K),
+            fill_color = if_else(Status == "Present", color_palette[as.numeric(K)], "#ffffff"))
+
+
+# Creates the plot ~
+ngsAdmix <-
+  ggplot(fulldfUp, aes(x = Sample_ID, y = Ancestry, fill = fill_color, pattern = Status), colour = "#000000") +
+  geom_col_pattern(width = .85, alpha = .7, pattern_size = .1, pattern_density = .01, pattern_spacing = .075, pattern_units = "in", pattern_colour = "#000000", pattern_fill = "#000000") +
+  facet_nested(chrtype + K_Value ~ Group + Population, scales = "free_x", space = "free",
+               strip = strip_nested(text_x = elem_list_text(size = c(15, 13), family = c("Optima", "Optima"), face = c("bold", "bold"), angle = c(0, 90), margins = c(1, 2, 3, 4)),
+                                    background_x = elem_list_rect(fill = c("#d6d6d6", "#FAFAFA"), colour = c("#000000", "#000000"), linewidth = c(.3, .3)),
+                                    by_layer_x = TRUE,
+                                    text_y = elem_list_text(size = c(15, 13), family = c("Optima", "Optima"), face = c("bold", "bold")),
+                                    background_y = elem_list_rect(fill = c("#d6d6d6", "#FAFAFA"), colour = c("#000000", "#000000"), linewidth = c(.3, .3)),
+                                    by_layer_y = TRUE)) +
+  scale_fill_identity() +
+  scale_pattern_manual(values = c("stripe", "none")) +
+  scale_x_discrete(expand = c(0, 0)) + 
+  scale_y_continuous(expand = c(0, 0), breaks = NULL) +
+  theme(panel.background = element_rect(fill = "#ffffff"),
+        panel.grid.minor.x = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.border = element_blank(),
+        panel.spacing.y = unit(.2, "cm"),
+        plot.title = element_blank(),
+        legend.position = "none",
+        axis.title = element_blank(),
+        axis.text.x.bottom = element_blank(),
+        #axis.text.x.bottom = element_text(colour = "#000000", face = "bold", angle = 90, vjust = .5, hjust = .5),
+        axis.text.y = element_blank(),
+        axis.ticks = element_blank())
+
+
+# Saves the final plot ~
+ggsave(ngsAdmix, file = "Y150239Genomics--ngsAdmix_OLD.pdf",
+       device = cairo_pdf, width = 20, height = 12, scale = 1, dpi = 600)
+#ggsave(ngsAdmix, file = "Y150239Genomics--ngsAdmix.jpeg",
+#       width = 20, height = 8, scale = 1, dpi = 600)
+
+
+#
+##
+### The END ~~~~~
